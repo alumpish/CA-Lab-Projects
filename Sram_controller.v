c02adeabda4dd599cad19c3df07debf54912a1ee
alumpish <apadyav@gmail.com>
2023-05-31 13:11:16 +0330

    Complete SRAM Controller

--- Sram_controller.v
+++ Sram_controller.v
@@ -1,116 +1,76 @@
 module SramController(
     input clk, rst,
     input wr_en, rd_en,
-    input[31:0] address,
+    input [31:0] address,
     input [31:0] writeData,
+    output reg [31:0] readData,
+    output reg ready,            // to freeze other stages
 
-    output[31:0] readData,
+    inout [15:0] SRAM_DQ,        // SRAM Data bus 16 bits
+    output reg [17:0] SRAM_ADDR, // SRAM Address bus 18 bits
+    output SRAM_UB_N,            // SRAM High-byte data mask
+    output SRAM_LB_N,            // SRAM Low-byte data mask
+    output reg SRAM_WE_N,        // SRAM Write enable
+    output SRAM_CE_N,            // SRAM Chip enable
+    output SRAM_OE_N             // SRAM Output enable
+);
+    assign {SRAM_UB_N, SRAM_LB_N, SRAM_CE_N, SRAM_OE_N} = 4'b0000;
 
-    output reg ready,
+    wire [31:0] memAddr;
+    assign memAddr = address - 32'd1024;
 
-    inout [15:0]SRAM_DQ,
-    output [17:0]SRAM_ADDR,
-    output reg SRAM_UB_N,
-    output reg SRAM_LB_N,
-    output reg SRAM_WE_N,
-    output reg SRAM_CE_N,
-    output reg SRAM_OE_N
-  );
+    wire [17:0] sramLowAddr, sramHighAddr;
+    assign sramLowAddr = memAddr[18:1];
+    assign sramHighAddr = sramLowAddr + 18'd1;
 
-  reg [2:0] ps, ns;
-  reg [17:0]addr;
-  reg [15:0]W_D_O_16;
-  reg [31:0]R_D_32;
+    reg [15:0] dq;
+    assign SRAM_DQ = wr_en ? dq : 16'bz;
 
-  parameter [2 : 0] IDLE = 3'b0;
-  parameter [2 : 0] WRITE_1 = 3'b001;
-  parameter [2 : 0] WRITE_2 = 3'b010;
-  parameter [2 : 0] WRITE_END = 3'b101;
-  parameter [2 : 0] READ_1 = 3'b001;
-  parameter [2 : 0] READ_2 = 3'b010;
-  parameter [2 : 0] READ_3 = 3'b011;
-  parameter [2 : 0] READ_END = 3'b101;
+    localparam Idle = 3'd0, DataLow = 3'd1, DataHigh = 3'd2, Finish = 3'd3, NoOp = 3'd4, Done = 3'd5;
+    reg [2:0] ns, ps;
 
+    always @(ps, wr_en, rd_en) begin
+        case (ps)
+            Idle: ns = (wr_en == 1'b1 || rd_en == 1'b1) ? DataLow : Idle;
+            DataLow: ns = DataHigh;
+            DataHigh: ns = Finish;
+            Finish: ns = NoOp;
+            NoOp: ns = Done;
+            Done: ns = Idle;
+        endcase
+    end
 
-  assign SRAM_DQ = (SRAM_WE_N == 0) ? W_D_O_16 : 16'bzzzzzzzzzzzzzzzz;
-  assign SRAM_ADDR = addr;
-  assign readData = R_D_32;
+    always @(*) begin
+        SRAM_ADDR = 18'b0;
+        SRAM_WE_N = 1'b1;
+        ready = 1'b0;
 
-  always@(posedge clk,posedge rst)
-  begin
-    if(rst)
-      ps <= IDLE;
-    else
-      ps <= ns;
-  end
+        case (ps)
+            Idle: ready = ~(wr_en | rd_en);
+            DataLow: begin
+                SRAM_ADDR = sramLowAddr;
+                SRAM_WE_N = ~wr_en;
+                dq = writeData[15:0];
+                if (rd_en)
+                    readData[15:0] <= SRAM_DQ;
+            end
+            DataHigh: begin
+                SRAM_ADDR = sramHighAddr;
+                SRAM_WE_N = ~wr_en;
+                dq = writeData[31:16];
+                if (rd_en)
+                    readData[31:16] <= SRAM_DQ;
+            end
+            Finish: begin
+                SRAM_WE_N = 1'b1;
+            end
+            NoOp:;
+            Done: ready = 1'b1;
+        endcase
+    end
 
-  always@(ps,wr_en,rd_en)
-  begin
-    if(ps == WRITE_END || ps == READ_END)
-      ns = IDLE;
-    else if(wr_en|rd_en)
-      ns = ps + 1;
-    else
-      ns = IDLE;
-  end
-
-  always@(ps,wr_en,rd_en,writeData)
-  begin
-    {SRAM_WE_N, SRAM_UB_N, SRAM_LB_N, SRAM_CE_N, SRAM_OE_N, ready} = 6'b100000;
-    if(wr_en)
-    case(ps)
-      IDLE:
-      begin
-        ready=~(wr_en|rd_en);
-      end
-      WRITE_1:
-      begin
-        W_D_O_16 = writeData[15:0];
-        addr = (address[17:0] - 32'd1024) >> 1;
-        SRAM_WE_N = 1'b0;
-      end
-      WRITE_2:
-      begin
-        W_D_O_16 = writeData[31:16];
-        addr = ((address[17:0] - 32'd1024) >> 1) + 1;
-        SRAM_WE_N = 1'b0;
-      end
-      WRITE_END:
-      begin
-        ready=1'b1;
-      end
-    endcase
-    else if(rd_en)
-    case(ps)
-      IDLE:
-      begin
-        ready=~(wr_en|rd_en);
-      end
-      READ_1:
-      begin
-        addr = (address[17:0] - 32'd1024) >> 1;
-      end
-      READ_2:
-      begin
-        R_D_32[15:0] = SRAM_DQ;
-        addr = ((address[17:0] - 32'd1024) >> 1) + 1;
-      end
-      READ_3:
-      begin
-        R_D_32[31:16] = SRAM_DQ;
-      end
-      READ_END:
-      begin
-        ready = 1'b1;
-      end
-    endcase
-    else
-    case(ps)
-      IDLE:
-      begin
-        ready = 1'b1;
-      end
-    endcase
-  end
-
+    always @(posedge clk or posedge rst) begin
+        if (rst) ps <= Idle;
+        else ps <= ns;
+    end
 endmodule
